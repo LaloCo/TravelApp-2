af6ec67cc0591b25587a97590cfecdf14b5c7cb0
andreea-tamaduianu <80282688+andreea-tamaduianu@users.noreply.github.com>
2021-03-15 09:51:02 +0200

    Update Constants.cs

--- TravelApp/TravelApp/Helpers/Constants.cs
+++ TravelApp/TravelApp/Helpers/Constants.cs
@@ -9,7 +9,7 @@ namespace TravelApp.Helpers
     public class Constants
     {
         public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
-        public const string CLIENT_ID = "";
-        public const string CLIENT_SECRET = "";
+        public const string CLIENT_ID = "BMUQCW5QX1EQ5IGAD5LQQEGDY3TCEWVEMBD42FGOQR2LGPYB";
+        public const string CLIENT_SECRET = "0CQX2GYZWXQU21WQHOFHZVHXAL3UAU10AEE3CHJXJJX5Y5BI";
     }
 }
